b21b40a685f5f4d7911cd418b402a37ca0703f4a
majid mohammadian <majeedmohammadian@gmail.com>
2024-03-10 02:37:18 +0330

    add country factory

---
+++ database/factories/CountryFactory.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace JobMetric\BanIp\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use JobMetric\Location\Models\LocationCountry;
+
+/**
+ * @extends Factory<LocationCountry>
+ */
+class CountryFactory extends Factory
+{
+    protected $model = LocationCountry::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->country,
+            'flag' => $this->faker->countryCode,
+            'mobile_prefix' => null,
+            'validation' => [],
+            'status' => $this->faker->boolean
+        ];
+    }
+
+    /**
+     * set name
+     *
+     * @param string $name
+     *
+     * @return static
+     */
+    public function setName(string $name): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'name' => $name
+        ]);
+    }
+
+    /**
+     * set flag
+     *
+     * @param string $flag
+     *
+     * @return static
+     */
+    public function setFlag(string $flag): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'flag' => $flag
+        ]);
+    }
+
+    /**
+     * set mobile prefix
+     *
+     * @param string $mobile_prefix
+     *
+     * @return static
+     */
+    public function setMobilePrefix(string $mobile_prefix): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'mobile_prefix' => $mobile_prefix
+        ]);
+    }
+
+    /**
+     * set validation
+     *
+     * @param string $validation
+     *
+     * @return static
+     */
+    public function setValidation(string $validation): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'validation' => $validation
+        ]);
+    }
+
+    /**
+     * set status
+     *
+     * @param string $status
+     *
+     * @return static
+     */
+    public function setStatus(string $status): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'status' => $status
+        ]);
+    }
+}
