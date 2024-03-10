cecee1e9b1f082d6c73f65c115001b2795410bf3
majid mohammadian <majeedmohammadian@gmail.com>
2024-03-10 23:59:07 +0330

    add language

---
+++ lang/en/base.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Location Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Location for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'validation' => [
+        'errors' => 'Validation errors occurred.',
+    ],
+
+    'messages' => [
+        'created' => 'The :name was created successfully.',
+        'updated' => 'The :name was updated successfully.',
+        'deleted' => 'The :name was deleted successfully.',
+    ],
+
+    'model_name' => [
+        'country' => 'Country',
+        'province' => 'Province',
+        'city' => 'City',
+        'district' => 'District',
+        'geo_area' => 'Geo Area',
+        'address' => 'Address',
+    ],
+
+];
