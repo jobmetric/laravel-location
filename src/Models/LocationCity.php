8e6d67c9761d0155ed3770eaf7ae13c5d83a9947
majid mohammadian <majeedmohammadian@gmail.com>
2024-03-11 02:12:52 +0330

    add relation in model location city

--- src/Models/LocationCity.php
+++ src/Models/LocationCity.php
@@ -4,15 +4,28 @@ namespace JobMetric\Location\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use JobMetric\PackageCore\Models\HasBooleanStatus;
 
 /**
+ * table properties
  * @property int id
  * @property int location_country_id
  * @property int location_province_id
  * @property string name
  * @property boolean status
+ *
+ * relationships properties
+ * @property string country_name
+ * @property string country_mobile_prefix
+ * @property string country_flag
+ * @property string country_validation
+ * @property boolean country_status
+ *
+ * @property string province_name
+ * @property boolean province_status
  */
 class LocationCity extends Model
 {
@@ -35,5 +48,65 @@ class LocationCity extends Model
     public function getTable()
     {
         return config('location.tables.city', parent::getTable());
+    }
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(LocationCountry::class);
+    }
+
+    public function province(): BelongsTo
+    {
+        return $this->belongsTo(LocationProvince::class);
+    }
+
+    public function districts(): HasMany
+    {
+        return $this->hasMany(LocationDistrict::class);
+    }
+
+    public function geoAreaZones(): HasMany
+    {
+        return $this->hasMany(LocationGeoAreaZone::class);
+    }
+
+    public function geoAreaZonesWithGeoArea(): HasMany
+    {
+        return $this->hasMany(LocationGeoAreaZone::class)->with('geoArea');
+    }
+
+    public function getCountryNameAttribute()
+    {
+        return $this->country->name;
+    }
+
+    public function getCountryMobilePrefixAttribute()
+    {
+        return $this->country->mobile_prefix;
+    }
+
+    public function getCountryFlagAttribute()
+    {
+        return $this->country->flag;
+    }
+
+    public function getCountryValidationAttribute()
+    {
+        return $this->country->validation;
+    }
+
+    public function getCountryStatusAttribute()
+    {
+        return $this->country->status;
+    }
+
+    public function getProvinceNameAttribute()
+    {
+        return $this->province->name;
+    }
+
+    public function getProvinceStatusAttribute()
+    {
+        return $this->province->status;
     }
 }
