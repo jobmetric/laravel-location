e4c70ac1b9bcd37e2d82410853567f5a1ec4fed0
majid mohammadian <majeedmohammadian@gmail.com>
2024-03-19 21:18:48 +0330

    add province events

---
+++ src/Events/Province/ProvinceDeleteEvent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace JobMetric\Location\Events\Province;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use JobMetric\Location\Models\LocationProvince;
+
+class ProvinceDeleteEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public readonly LocationProvince $locationProvince
+    )
+    {
+    }
+}
---
+++ src/Events/Province/ProvinceUpdateEvent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace JobMetric\Location\Events\Province;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use JobMetric\Location\Models\LocationProvince;
+
+class ProvinceUpdateEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public readonly LocationProvince $locationProvince,
+        public readonly array            $data
+    )
+    {
+    }
+}
