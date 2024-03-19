15ddda41abacdd090d213cea73c3c8cbb9690f81
majid mohammadian <majeedmohammadian@gmail.com>
2024-03-19 21:31:05 +0330

    add district facade

---
+++ src/Facades/LocationDistrict.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JobMetric\Location\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static \Spatie\QueryBuilder\QueryBuilder query(array $filter = [])
+ * @method static \Illuminate\Pagination\LengthAwarePaginator paginate(array $filter = [], int $page_limit = 15)
+ * @method static \Illuminate\Database\Eloquent\Collection all(array $filter = [])
+ * @method static array store(array $data)
+ * @method static array update(int $location_district_id, array $data)
+ * @method static array delete(int $location_district_id)
+ *
+ * @see \JobMetric\Location\Services\DistrictManager
+ */
+class LocationDistrict extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return \JobMetric\Location\Services\DistrictManager::class;
+    }
+}
