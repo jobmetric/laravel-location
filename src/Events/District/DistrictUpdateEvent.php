be887cadeaedc531c7ecdb88c249ca2f62b349b9
majid mohammadian <majeedmohammadian@gmail.com>
2024-03-19 21:23:50 +0330

    add district events

---
+++ src/Events/District/DistrictUpdateEvent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace JobMetric\Location\Events\District;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use JobMetric\Location\Models\LocationDistrict;
+
+class DistrictUpdateEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public readonly LocationDistrict $locationDistrict,
+        public readonly array            $data
+    )
+    {
+    }
+}
