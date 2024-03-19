abcc1bc6f9a7586c646812caeef967cad94dacda
majid mohammadian <majeedmohammadian@gmail.com>
2024-03-19 21:10:53 +0330

    change facade method

--- src/Facades/LocationCountry.php
+++ src/Facades/LocationCountry.php
@@ -5,6 +5,9 @@ namespace JobMetric\Location\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Spatie\QueryBuilder\QueryBuilder query(array $filter = [])
+ * @method static \Illuminate\Pagination\LengthAwarePaginator paginate(array $filter = [], int $page_limit = 15)
+ * @method static \Illuminate\Database\Eloquent\Collection all(array $filter = [])
  * @method static array store(array $data)
  * @method static array update(int $location_country_id, array $data)
  * @method static array delete(int $location_country_id)
