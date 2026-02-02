87310fa60f6ea082e7664c28085dab7493b0e6bc
majidmohammadian <majeedmohammadian@gmail.com>
2026-02-02 12:34:00 +0330

    added test case file

---
+++ tests/TestCase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace JobMetric\Location\Tests;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use JobMetric\EventSystem\EventSystemServiceProvider;
+use JobMetric\Flow\FlowServiceProvider;
+use JobMetric\Language\LanguageServiceProvider;
+use JobMetric\Language\Models\Language;
+use JobMetric\Translation\TranslationServiceProvider;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    use RefreshDatabase;
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            EventSystemServiceProvider::class,
+            LanguageServiceProvider::class,
+            TranslationServiceProvider::class,
+            FlowServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        loadMigrationPath(__DIR__ . '/database/migrations');
+
+        if (! Language::query()->exists()) {
+            Language::factory()->english()->create();
+        }
+    }
+}
