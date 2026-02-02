02c89458613c4dc188015d3be0a1eb41b39c5418
majidmohammadian <majeedmohammadian@gmail.com>
2026-02-02 13:41:20 +0330

    changed CheckExistNameRule for better write code

--- src/Rules/CheckExistNameRule.php
+++ src/Rules/CheckExistNameRule.php
@@ -3,6 +3,7 @@
 namespace JobMetric\Location\Rules;
 
 use Closure;
+use Illuminate\Contracts\Validation\DataAwareRule;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
@@ -12,7 +13,7 @@ use JobMetric\Location\Models\City;
 use JobMetric\Location\Models\District;
 use JobMetric\Location\Models\Province;
 
-class CheckExistNameRule implements ValidationRule
+class CheckExistNameRule implements ValidationRule, DataAwareRule
 {
     /**
      * Map of model => its parent foreign key column.
@@ -38,6 +39,27 @@ class CheckExistNameRule implements ValidationRule
     }
 
     /**
+     * The full data under validation (provided by the validator).
+     *
+     * @var array<string, mixed>
+     */
+    protected array $data = [];
+
+    /**
+     * Set the data under validation.
+     *
+     * @param array<string, mixed> $data
+     *
+     * @return $this
+     */
+    public function setData(array $data): static
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
      * Run the validation rule.
      *
      * @param Closure(string): PotentiallyTranslatedString $fail
@@ -55,10 +77,18 @@ class CheckExistNameRule implements ValidationRule
             $query->where('id', '!=', $this->object_id);
         }
 
-        if ($this->parent_id !== null) {
-            $parentKey = self::PARENT_KEY_MAP[$this->model] ?? null;
-            if ($parentKey) {
-                $query->where($parentKey, $this->parent_id);
+        $parentKey = self::PARENT_KEY_MAP[$this->model] ?? null;
+        if ($parentKey) {
+            $parentId = $this->parent_id;
+
+            // When this rule is used inside dto()/FormRequest validation, the request object
+            // may not provide input() as expected; derive the parent id from validated payload.
+            if ($parentId === null && array_key_exists($parentKey, $this->data)) {
+                $parentId = is_numeric($this->data[$parentKey]) ? (int) $this->data[$parentKey] : null;
+            }
+
+            if ($parentId !== null) {
+                $query->where($parentKey, $parentId);
             }
         }
 
